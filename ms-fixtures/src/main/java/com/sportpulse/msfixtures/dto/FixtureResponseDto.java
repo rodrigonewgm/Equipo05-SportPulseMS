f704e7ad9d2697a3521acdc981fb3e0ff7883d38
Rodrigo Leal <rodrigonewgm@gmail.com>
2026-04-28 03:56:19 -0300

    ms-fixtures: agrego DTOs y corrijo URL de API externa

---
+++ ms-fixtures/src/main/java/com/sportpulse/msfixtures/dto/FixtureResponseDto.java
@@ -0,0 +1,19 @@
+package com.sportpulse.msfixtures.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class FixtureResponseDto {
+    private Integer id;
+    private String date;
+    private FixtureStatusDto status;
+    private FixtureLeagueDto league;
+    private TeamInFixtureDto homeTeam;
+    private TeamInFixtureDto awayTeam;
+    private VenueDto venue;
+}
+}
