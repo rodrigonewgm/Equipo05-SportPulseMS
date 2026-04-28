ef7c2292a2f2d6a3b83a61de11d15eb177bf93d2
Rodrigo Leal <rodrigonewgm@gmail.com>
2026-04-28 04:10:43 -0300

    ms-fixtures: agrego configuración, JWT y WebFlux

---
+++ ms-fixtures/src/main/java/com/sportpulse/msfixtures/config/ApiFootballConfig.java
@@ -0,0 +1,24 @@
+package com.sportpulse.msfixtures.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class ApiFootballConfig {
+
+    @Value("${rapidapi.key}")
+    private String apiKey;
+
+    @Value("${rapidapi.base-url}")
+    private String baseUrl;
+
+    @Bean
+    public WebClient apiFootballWebClient() {
+        return WebClient.builder()
+                .baseUrl(baseUrl)
+                .defaultHeader("x-apisports-key", apiKey)
+                .build();
+    }
+}
